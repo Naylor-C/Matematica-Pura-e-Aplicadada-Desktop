03415a9a9584774554f9aabafecfcf8a06b5be0c
Naylor-C <jhonatanproject1997@gmail.com>
2025-03-25 14:54:00 -0300

    x

---
+++ main/frame.cc
@@ -0,0 +1,23 @@
+#include <gtkmm.h>
+
+
+class Frame : public Gtk::Window {
+ public Frame();
+
+};
+
+
+Frame::Frame() {
+  
+   set_title("App");
+   set_default_size(800, 600);
+
+};
+
+
+int main (int argc, char* argv[]) {
+
+   auto app = Gtk::Application::create("org.gtkmm.examples.base");
+
+   return app->make_window_and_run<Frame> (argc, argv);
+};
